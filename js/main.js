3aab9343c5494cc4af99ea9d05056f37e52ac782
o黯淡o <york8817612@hotmail.com>
2021-03-14 10:31:50 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,3 +6,22 @@ window.onload = () => {
              .register('./sw.js');
   }
 }
+
+function ROStart( type, element )
+{
+	window.open( 'http://demo.robrowser.com/?demo/2.7/', '_blank', [
+		'directories=0',
+		'fullscreen=0',
+		'top='  + ( (window.innerHeight||document.body.clientHeight)-600) / 2,
+		'left=' + ( (window.innerWidth ||document.body.clientWidth) -800) / 2,
+		'height='+ 600,
+		'width=' + 800,
+		'location=0',
+		'menubar=0',
+		'resizable=0',
+		'scrollbars=0',
+		'status=0',
+		'toolbar=0'
+	].join(','));
+}
+
